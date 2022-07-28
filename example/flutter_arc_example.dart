bbf6a8527663b7078388d85b58257c962985ef88
@loljoda <loaij64@gmail.com>
2022-07-28 14:19:24 +0300

    first commit

---
+++ example/flutter_arc_example.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_arc/flutter_arc.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/flutter_arc_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_arc/flutter_arc.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
